2fa10de2f4ad685296196486446f13f5e0cbd396
ACG-Q <liuli990206@gmail.com>
2021-08-02 15:15:08 +0800

    修改 Steam创意工坊mod下载工具

--- 『分类』下载辅助/Steam创意工坊mod下载工具.user.js
+++ 『分类』下载辅助/Steam创意工坊mod下载工具.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam创意工坊mod下载工具
 // @namespace    
-// @version      1.0
+// @version      1.2
 // @description  在创意工坊中注入一个MOD下载按钮，用于下载MOD，支持下载合集, API来源：https://steamworkshopdownloader.io/
 // @author       六记(原:极品小猫)
 // @include      https://steamcommunity.com/sharedfiles/filedetails/*
@@ -10,13 +10,27 @@
 // @match        https://steamworkshopdownloader.io/*
 // @connect      *.steamworkshopdownloader.io
 // @grant        GM_setClipboard
-// @grant        GM_addStyle
 // @grant        GM_download
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 (function() {
     'use strict';
+    
+    // 控制API链接
+    let urls = [
+        "https://backend-01-prd.steamworkshopdownloader.io/api/details/file",
+        "https://backend-02-prd.steamworkshopdownloader.io/api/details/file",
+        "https://backend-03-prd.steamworkshopdownloader.io/api/details/file"
+    ]
+    let getUUids = [
+        "https://backend-01-prd.steamworkshopdownloader.io/api/download",
+        "https://backend-02-prd.steamworkshopdownloader.io/api/download",
+        "https://backend-03-prd.steamworkshopdownloader.io/api/download"
+      
+    ]
+    let TIMEOUT = 60;// 秒
+    let startMid = "",collectionName = "";
     let webHost=location.host, webPath=location.pathname;
 
     if(webHost=='steamcommunity.com') {
@@ -55,39 +69,61 @@
 
             document.querySelector('#ItemControls').appendChild(down_span);
             document.querySelector('#download_ModBtn').addEventListener('click', function(){
+                startMid = publishedfileid
                 ModDownloadAjax(publishedfileid);
             });
         }
         
         // https://backend-01-prd.steamworkshopdownloader.io/api/details/file
         // POST []
-        function ModDownloadAjax(mid){
+        function ModDownloadAjax(mid, url, isCollection){
+            isCollection = isCollection?isCollection:true;
+            url = url?url:urls[0]
             GM_xmlhttpRequest({
-                url:'https://backend-01-prd.steamworkshopdownloader.io/api/details/file',
+                url: url,
                 method: 'post',
                 data: `[${mid}]`,
                 responseType : 'json',
                 onload: function(e){
                     console.log(e);
                     if(!e.response){
-                        alert(`获取MOD失败`)
+                        console.log(`获取MOD失败 启动最后的手段`)
+                        endModDown(startMid, `[workshop-${startMid}]${collectionName?collectionName:""}.zip`)
                         return
                     }
                     let data=e.response[0], downloadTitle=`[workshop-${mid}]${data.title}.zip`;
                     // 是否为合集
-                    if(data.children){
+                    if(data.children && isCollection){
+                        collectionName = data.title
                         data.children.map((items)=>{
                             let mid = items.publishedfileid
-                            ModDownloadAjax(mid)
+                            ModDownloadAjax({mid:mid,isCollection:false})
                         })
                     }else if(data.file_url){
                         GM_download({
                             url: data.file_url,
                             name: downloadTitle,
-                            saveAs : true,
+                            saveAs: true,
+                            timeout: TIMEOUT * 1000,
+                            ontimeout: function(){
+                                console.log(`超时...`)
+                                // 将文件名设置到系统剪贴板
+                                GM_setClipboard(downloadTitle)
+                                console.log("复制完成, 打开下载链接")
+                                // 打开下载链接
+                                window.open(data.file_url)
+                            }
                         });
                     }else{
-                        alert(`获取MOD失败`)
+                        let nextUrlIndex = urls.indexOf(url) + 1
+                        if(nextUrlIndex < urls.length){
+                            console.log(`${url} --> 获取MOD失败 正在切换至 ${urls[nextUrlIndex]} 进行尝试`)
+                            ModDownloadAjax(mid, urls[nextUrlIndex])
+                        }else{
+                            console.log(`${url} --> 获取MOD失败 启动最后的手段`)
+                            endModDown(mid, downloadTitle)
+                        }
+                        
                     }
                 },
                 onerror: function(e){
@@ -95,94 +131,53 @@
                 }
             });
         }
-    } else if(webHost=='t.vvwall.com') {
-        GM_addStyle(`
-input[name="mid"]{
-padding: 0 10px;
-}
-`);
-        $('button.starts').click(function(){
-            addMObserver('#des', function(){
-                console.log($('code#des').text());
-                $('code#des').html(markdown.toHTML($('code#des').text()));
-            });
-        });
-        let workshop_A=$('<a id="workshop">').text('创意工坊');
-        let dlbtn=$('#dlbtn').removeAttr('onclick target').click(function(e){
-            let inputText=$('input[name="mid"]').val().trim(),
-                id=getUrlParam('id', inputText, inputText),
-                modTitle=$('#modtitle').text().trim(),
-                downloadTitle="[workshop-"+id+"]"+modTitle+".zip";
-            workshop_A.attr({'href':'https://steamcommunity.com/sharedfiles/filedetails/?id='+id});
-
-            GM_download({
-                url: e.href,
-                name: downloadTitle,
-                saveAs : true,
-            });
-            e.preventDefault(); //阻止默认动作
-            e.stopPropagation(); //阻止冒泡
-        });
-        $('.downloadbox').prepend(dlbtn);
-        dlbtn.after(workshop_A);
-    }
-
-        if(location.hash) {
-            $('input[name="mid"]').val(location.hash.replace('#',''));
-            $('button.starts').click();
+      
+        function endModDown(mid, downloadTitle, url){
+          let data = `{"publishedFileId":${mid},"collectionId":null,"extract":false,"hidden":false,"direct":false,"autodownload":false}`
+          url = url?url:getUUids[0]
+          GM_xmlhttpRequest({
+              url: `${url}/request`,
+              method: 'post',
+              data: data,
+              responseType : 'json',
+              onload: function(e){
+                  console.log(e);
+                  if(!e.response){
+                      alert(`获取UUID失败`)
+                      return
+                  }
+                  console.log(`获取UUID成功\nmid: ${mid}\nuuid: ${e.response.uuid}\ntitle: ${downloadTitle}`)
+                  if(e.response.uuid){
+                      console.log(`${url}/transmit?uuid=${e.response.uuid}`)
+                      GM_download({
+                          url: `${url}/transmit?uuid=${e.response.uuid}`,
+                          name: downloadTitle,
+                          saveAs: true,
+                          timeout: TIMEOUT * 1000,
+                          ontimeout: function(){
+                              console.log(`超时...`)
+                              // 将文件名设置到系统剪贴板
+                              GM_setClipboard(downloadTitle)
+                              console.log("复制完成, 打开下载链接")
+                              // 打开下载链接
+                              window.open(`${url}/transmit?uuid=${e.response.uuid}`)
+                          }
+                      });
+                  }else{
+                      let nextUrlIndex = getUUids.indexOf(url) + 1
+                      if(nextUrlIndex < getUUids.length){
+                            console.log(`${url} --> 获取UUID失败 正在切换至 ${getUUids[nextUrlIndex]} 进行尝试`)
+                            ModDownloadAjax(mid, downloadTitle, getUUids[nextUrlIndex])
+                        }else{
+                            alert(`${url} --> 获取UUID失败 ==> 下载MOD失败`)
+                        }
+                  }
+              },
+              onerror: function(e){
+                  console.error('error', e);
+              }
+          })
         }
-    // Your code here...
-    function addMObserver(selector, callback, Kill, option) {
-        var watch = document.querySelector(selector);
-
-        if (!watch) {
-            return;
-        }
-        console.warn('watch:', watch, selector);
-        var observer = new MutationObserver(function(mutations){
-            console.log('mutations:', mutations);
-            var nodeAdded = mutations.some(function(x){
-                console.log(x);
-                return x.addedNodes.length > 0;
-            });
-            console.log(nodeAdded);
-            if (nodeAdded) {
-                console.log(mutations, observer);
-                callback(mutations, observer);
-                if(Kill) {
-                    console.log('停止'+selector+'的监控');
-                    observer.disconnect();
-                }
-            }
-        });
-        observer.observe(watch, option||{childList: true, subtree: true});//
-    }
-
-    function getUrlParam(name, url, option, newVal) {//筛选参数，url 参数为数字时
-        var search = url ? url.replace(/^.+\?/,'') : location.search;
-        //网址传递的参数提取，如果传入了url参数则使用传入的参数，否则使用当前页面的网址参数
-        var reg = new RegExp("(?:^|&)(" + name + ")=([^&]*)(?:&|$)", "i");		//正则筛选参数
-        var str = search.replace(/^\?/,'').match(reg);
-
-        console.log(str, option);
-
-        if (str !== null) {
-            switch(option) {
-                case 0:
-                    return unescape(str[0]);		//所筛选的完整参数串
-                case 1:
-                    return unescape(str[1]);		//所筛选的参数名
-                case 2:
-                    return unescape(str[2]);		//所筛选的参数值
-                case 'new':
-                    return url.replace(str[1]+'='+str[2], str[1]+'='+newVal);
-                default:
-                    return unescape(str[2]);        //默认返回参数值
-            }
-        } else if(!str && option) {
-            return option;
-        } else {
-            return null;
-        }
-    }
+    
+    } 
 })();
